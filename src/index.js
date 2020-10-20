e74f58f85d80527c077597c12ff6f37bc99e1f41
gabrielhicks <gabrielhicks@mail.com>
2020-10-20 13:53:04 -0400

    recording mic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,87 @@
 document.addEventListener("DOMContentLoaded", function() {
     keyHandler()
     soundHandler()
+    getUserMedia()
 })
 
+function getUserMedia() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        console.log('getUserMedia supported.');
+    navigator.mediaDevices.getUserMedia (
+      // constraints - only audio needed for this app
+    {
+        audio: true
+    })
+      // Success callback
+    .then(function(stream) {
+    const mediaRecorder = new MediaRecorder(stream);
+    const record = document.querySelector('.record');
+    const stop = document.querySelector('.stop');
+    record.onclick = function() {
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+    record.style.background = "red";
+    record.style.color = "black";
+    }
+
+    let chunks = [];
+
+    mediaRecorder.ondataavailable = function(e) {
+    chunks.push(e.data);
+    }
+
+    stop.onclick = function() {
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+    record.style.background = "";
+    record.style.color = "";
+    }
+    mediaRecorder.onstop = function(e) {
+    console.log("recorder stopped2");
+    const soundClips = document.querySelector('.sound-clips');
+    const clipName = prompt('Enter a name for your sound clip');
+
+    const clipContainer = document.createElement('article');
+    const clipLabel = document.createElement('p');
+    const audio = document.createElement('audio');
+    const deleteButton = document.createElement('button');
+
+    clipContainer.classList.add('clip');
+    audio.setAttribute('controls', '');
+    deleteButton.innerHTML = "Delete";
+    clipLabel.innerHTML = clipName;
+
+    clipContainer.appendChild(audio);
+    clipContainer.appendChild(clipLabel);
+    clipContainer.appendChild(deleteButton);
+    soundClips.appendChild(clipContainer);
+
+    const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+    chunks = [];
+    const audioURL = window.URL.createObjectURL(blob);
+    audio.src = audioURL;
+
+    deleteButton.onclick = function(e) {
+    let evtTgt = e.target;
+    evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+    }
+}
+    })
+      // Error callback
+    .catch(function(err) {
+        console.log('The following getUserMedia error occured: ' + err);
+    }
+);
+    } else {
+        console.log('getUserMedia not supported on your browser!');
+    }
+    
+}
+
 function soundHandler() {
+
     function playAudio(keyCode){
         var audio = new Audio('media/'+keyCode+'.mp3');
         audio.play();
