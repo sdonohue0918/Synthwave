4db140b167722c591a18354d5a020257894cfccc
sdonohue0918 <sdonohue0918@gmail.com>
2020-10-20 13:18:16 -0400

    serious refactoring needed but just testing recording functionality. need to see if we can get raw data now

--- src/buffer_functions.js
+++ src/buffer_functions.js
@@ -1,29 +1,30 @@
-let wavBuffer = null
+// let wavBuffer = null
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext
+// window.AudioContext = window.AudioContext || window.webkitAudioContext
 
-let context = new AudioContext()
+// let context = new AudioContext()
 
-function loadSound(url) {
-    let request = new XMLHttpRequest()
-    request.open('GET', url, true)
-    request.responseType = 'arraybuffer'
+// function loadSound(url) {
+//     let request = new XMLHttpRequest()
+//     request.open('GET', url, true)
+//     request.responseType = 'arraybuffer'
 
-    request.onload = function() {
-        context.decodeAudioData(request.response, function(buffer) {
-            wavBuffer = buffer
-        }, onerror = function() {
-            console.log("error")
-        })
-    }
-    request.send()
-    console.log(request)
-}
+//     request.onload = function() {
+//         context.decodeAudioData(request.response, function(buffer) {
+//             wavBuffer = buffer
+//         }, onerror = function() {
+//             console.log("error")
+//         })
+//     }
+//     console.log(request.response)
+//     request.send()
+    
+// }
 
 
-function playSound(buffer, context) {
-    let source = context.createBufferSource()
-    source.buffer = buffer
-    source.connect(context.destination)
-    source.start(0)
-}
+// function playSound(buffer, context) {
+//     let source = context.createBufferSource()
+//     source.buffer = buffer
+//     source.connect(context.destination)
+//     source.start(0)
+// }
--- src/index.js
+++ src/index.js
@@ -3,29 +3,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    // keyListener()
-    // keyUpListener()
     
-    
-    //Audio Stream
     window.AudioContext = window.AudioContext || window.webkitAudioContext
     let context = new AudioContext()
     console.log(context)
     const audioStream = context.createMediaStreamDestination()
     const audioRecorder = new MediaRecorder(audioStream.stream)
 
-    //Buffer Loader
-    // let bufferloader = new BufferLoader(
-    //     context,
-    //     ['/Users/amydonohue/Flatiron/project3/proj3_d1/Synthwave/legowelt/SYNTH-MiniMoogStab1.wav',
-    // '/Users/amydonohue/Flatiron/project3/proj3_d1/Synthwave/legowelt/SYNTH-MiniMoogStab2.wav'],
-    // finishLoading
-    // )
-
-    // bufferloader.load()
-
-
-    //Recording Functions
+   keyListener()
+   keyUpListener()
 
     audioRecorder.addEventListener('dataavailable', function(e) {
         let dataChunks = []
@@ -44,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-    audioRecorder.addEventListener('stop', e => {
+    audioRecorder.addEventListener('stop', () => {
         let blob = newBlob(dataChunks, {'type': 'audio/ogg; codecs=opus'})
         const url = URL.createObjectURL(blob)
         const audioElement = document.querySelector('audio')
@@ -66,41 +52,35 @@ document.addEventListener('click', e => {
 
 
         
-    } else if (e.target.matches('#stop')) {
-        audioRecorder.stop()
+    }  else if (e.target.matches('#start')) {
+        e.target.id = 'stop'
 
+       
         
-
-    } else if (e.target.matches('#buffer')) {
-        loadSound('https://s3-us-west-2.amazonaws.com/s.cdpn.io/858/outfoxing.mp3')
     
     }
 })
 
 
-
- function finishLoading(soundArray) {
-        let src1 = context.createBufferSource()
-        let src2 = context.createBufferSource()
-        src1.buffer = soundArray[0]
-        src2.buffer = soundArray[1]
-        src1.connect(context.destination)
-        src2.connect(context.destination)
-
-        document.addEventListener('keydown', e => {
-            if (e.key === 'm') {
-                context.resume()
-            } else if (e.key === 'a') {
-                src1.start()
-            } else if (e.key === 's') {
-                src2.start()
-            }
-        })
+function playAudio(source) {
+   let options = { audioBitsPerSecond : 32000 }
+        const audio = new Audio(source)
+        audio.play()
+        const audioStream = audio.captureStream()
+        
+        const destination = context.createMediaStreamDestination()
+        const recorder = new MediaRecorder(destination.stream, options)
+        recorder.start()
+        console.log(recorder)
     }
+    
+    
+
+ 
             
            
 function keyUpListener() {
-        document.addEventListener('keydown', e => {
+        document.addEventListener('keyup', e => {
             if(e.key === 'm') {
                 context.resume()
                 
@@ -109,6 +89,7 @@ function keyUpListener() {
                 const note = document.getElementById("C")
                 note.dataset.status = "nopress"
                 note.style.color = "black"
+                
                 
                 
             } else if (e.key === 'w') {
@@ -207,94 +188,92 @@ function keyUpListener() {
 
 
     
-    
-    document.addEventListener('click', e => {
-        if (e.target.matches('#start')) {
-            
-            audioRecorder.start()
-            
 
-
-            
-        } else if (e.target.matches('#stop')) {
-            audioRecorder.stop()
-
-            
-
-        }
-    })
     
     
 
     function keyListener() {
 
 
-        document.addEventListener('keyup', e => {
+        document.addEventListener('keydown', e => {
             if (e.key === 'a') {
                 
                 const note = document.getElementById("C")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.C3.mp3")
             } else if (e.key === 'w') {
                 
                 const note = document.getElementById("Db")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.Db3.mp3")
             } else if (e.key === 's') {
                 
                 const note = document.getElementById("D")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.D3.mp3")
             } else if (e.key === 'e') {
                 
                 const note = document.getElementById("Eb")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.Eb3.mp3")
             } else if (e.key === 'f') {
                 
                 const note = document.getElementById("E")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.E3.mp3")
             } else if (e.key === 'g') {
                 
                 const note = document.getElementById("F")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.F3.mp3")
             } else if (e.key === 'h') {
                 
                 const note = document.getElementById("Gb")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.Gb3.mp3")
             } else if (e.key === 'u') {
                 
                 const note = document.getElementById("G")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.G3.mp3")
             } else if (e.key === 'j') {
                 
                 const note = document.getElementById("Ab")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.Ab3.mp3")
             } else if (e.key === 'i') {
                 
                 const note = document.getElementById("A")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.A3.mp3")
             } else if (e.key === 'k') {
                 
                 const note = document.getElementById("Bb")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.Bb3.mp3")
             } else if (e.key === 'l') {
                 
                 const note = document.getElementById("B")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.B3.mp3")
             } else if (e.key === 'p') {
                 
                 const note = document.getElementById("C1")
                 note.dataset.status = "pressed"
                 note.style.color = "yellow"
+                playAudio("./piano_mf/Piano.mf.C3.mp3")
             }
         })
 
